6e7d2e079435e3ee01c8c7fae38c78e80f696f64
Viraj Bangari <Viraj3f@gmail.com>
2016-05-12 00:38:54 -0400

    add setup module

---
+++ setup.py
@@ -0,0 +1,9 @@
+import sys
+
+try:
+    __import__("secrets")
+except ImportError:
+    print("no secrets.py file created, please do that with a DATABASE_URI")
+    sys.exit(-1)
+finally:
+    print("secrets.py exists!")
