45276f0f23dcb6225055d2ad1f9407e74281265e
Viraj Bangari <Viraj3f@gmail.com>
2016-05-23 16:59:04 -0400

    bugfixes

--- conferenceX/static/js/index.js
+++ conferenceX/static/js/index.js
@@ -21,5 +21,12 @@ $(document).ready(function(){
         }
     );
 
+    $("#google-maps").click(function() {
+        $("#google-maps iframe").css("pointer-events", "auto");
+    });
+
+    $("#google-maps").mouseleave(function() {
+        $("#google-maps iframe").css("pointer-events", "none");
+    });
 });
 
--- tests.py
+++ tests.py
@@ -5,7 +5,6 @@ import argparse
 import conferenceX.tests.dbtest as dbtest
 from conferenceX.tests.permissiontest import PermissionTest
 
-
 def run():
     """ runs testing script """
     parser = argparse.ArgumentParser(description="Do some tests")
