4db5f4ca4271274dcaf7135a0423a9b47cef2bab
Viraj Bangari <Viraj3f@gmail.com>
2016-05-25 16:51:12 -0400

    allow for min and int as jinnja methods

--- conferenceX/flask_app.py
+++ conferenceX/flask_app.py
@@ -4,10 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = SECRET_KEY
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
-
 db = SQLAlchemy(app)
+
+app.jinja_env.globals.update(min=min)
+app.jinja_env.globals.update(int=int)
 
 import conferenceX.views
